0db01286021fae8e991ae81673f3ad9d5b43d3d6
PerisOduol <oduolpepe618@gmail.com>
2023-05-31 17:35:01 +0300

    prepare headers

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,21 +1,12 @@
 // Our main file for making API calls
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '1559284210msh9201de8ba996a92p1b265djsn24a95e1305cf',
-      'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://shazam.p.rapidapi.com/charts/track', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam.p.rapidapi.com'
+        baseUrl: 'https://shazam.p.rapidapi.com',
+        prepareHeaders:() => {
+            headers.set('X-RapidAPI-Key','1559284210msh9201de8ba996a92p1b265djsn24a95e1305cf')
+        }
     })
 });
